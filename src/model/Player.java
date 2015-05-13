6131a3461cfbaf2cfa527683d2aa8e33975a05d7
0Freeicecream <0freeicecream@gmail.com>
2015-05-13 08:49:59 -0700

    Added Files
    Added current work done for the project

---
+++ src/model/Player.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+	
+	//class instance variables
+	private String name;
+	
+
+	public Player() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
