b17d4246a54ba7323ab89d0522b2fdbc6cc1d6c6
0Freeicecream <0freeicecream@gmail.com>
2015-05-15 08:46:06 -0700

    Added getters and setters for totalMoves and playerCoordniates Split playerCoordinates into playerCoordinatesX and playerCoordinatesY.

--- theGreatandSpaceousBuilding/src/byui/cit260/theGreatandSpaceousBuilding/model/Game.java
+++ theGreatandSpaceousBuilding/src/byui/cit260/theGreatandSpaceousBuilding/model/Game.java
@@ -1,10 +1,79 @@
 package byui.cit260.theGreatandSpaceousBuilding.model;
 
-public class Game {
+import java.io.Serializable;
+
+public class Game implements Serializable {
     
     /**
      * @author Ted
      * 
      */
-        
+    
+    //class instance variables
+    private double totalTime;
+    private double playerCoordinatesX;
+    private double playerCoordinatesY;
+
+    public Game() {
+    }
+
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(double totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public double getPlayerCoordinatesX() {
+        return playerCoordinatesX;
+    }
+
+    public void setPlayerCoordinatesX(double playerCoordinatesX) {
+        this.playerCoordinatesX = playerCoordinatesX;
+    }
+
+    public double getPlayerCoordinatesY() {
+        return playerCoordinatesY;
+    }
+
+    public void setPlayerCoordinatesY(double playerCoordinatesY) {
+        this.playerCoordinatesY = playerCoordinatesY;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "totalTime=" + totalTime + ", playerCoordinatesX=" + playerCoordinatesX + ", playerCoordinatesY=" + playerCoordinatesY + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.totalTime) ^ (Double.doubleToLongBits(this.totalTime) >>> 32));
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.playerCoordinatesX) ^ (Double.doubleToLongBits(this.playerCoordinatesX) >>> 32));
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.playerCoordinatesY) ^ (Double.doubleToLongBits(this.playerCoordinatesY) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.totalTime) != Double.doubleToLongBits(other.totalTime)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.playerCoordinatesX) != Double.doubleToLongBits(other.playerCoordinatesX)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.playerCoordinatesY) != Double.doubleToLongBits(other.playerCoordinatesY)) {
+            return false;
+        }
+        return true;
+    }
+    
 }
