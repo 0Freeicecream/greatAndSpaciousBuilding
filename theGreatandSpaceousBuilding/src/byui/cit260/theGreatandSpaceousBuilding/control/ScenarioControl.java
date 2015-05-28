5d39a6a57957a48c9610ce045b5f377e49ca9368
0Freeicecream <0freeicecream@gmail.com>
2015-05-28 10:38:30 -0700

    Partial code for ScenarioControl to share with partner while we figure out random numbers (because Java requires something that we're having a hard time with)

--- theGreatandSpaceousBuilding/src/byui/cit260/theGreatandSpaceousBuilding/control/ScenarioControl.java
+++ theGreatandSpaceousBuilding/src/byui/cit260/theGreatandSpaceousBuilding/control/ScenarioControl.java
@@ -5,10 +5,46 @@
  */
 package byui.cit260.theGreatandSpaceousBuilding.control;
 
+import java.util.Random;
 /**
  *
  * @author Néna and Ted
  */
 public class ScenarioControl {
+    
+    public class Random {
+        private int randNum() {
+            Random rn = new Random();
+            int num = rn.nextInt(10) + 1;
+            return num;
+        }
+
+        private int nextInt(int i) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    public int fingerOfScorn(int testimony, int obedience, int challengeRating) {
+        if (testimony > 99) {
+            //Error – Testimony too high
+            return -1;
+        } else if (testimony < 1) {
+            //Error – testimony too low
+            return -1;
+        } else if (obedience > 99) {
+            //Error – obedience too high
+            return -1;
+        } else if (obedience < 1) {
+            //Error – obedience too low
+            return -1;
+        } else if (challengeRating > 99) {
+            //Error – obedience too high
+            return -1;
+        } else if (challengeRating < 1) {
+            //Error – obedience too low
+            return -1;
+        } else
+            return (testimony + num1 ) - (challengeRating + Math.random() - obedience);
+    }
+    
     
 }
