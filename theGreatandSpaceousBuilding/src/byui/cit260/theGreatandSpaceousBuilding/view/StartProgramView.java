bf30ebe178416c01b4240753273e7aa52c8a8273
ngriley <fai11004@byui.edu>
2015-06-01 22:39:04 -0700

    added View Package, started beginning of  "startProgram()".

---
+++ theGreatandSpaceousBuilding/src/byui/cit260/theGreatandSpaceousBuilding/view/StartProgramView.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theGreatandSpaceousBuilding.view;
+
+/**
+ *
+ * @author Néna
+ */
+public class StartProgramView {
+    
+    public void startProgram(){
+            
+     
+        // DISPLAY the banner screen
+        this.displayBanner();
+        // Get the players name
+        // Create a new player
+        // DISPLAY a customized welcome message
+        // DISPLAY the main menu
+      
+}
+
+    private void displayBanner() {
+        System.out.println("We are going to tell you how to play this game");
+    }
+    
+}
