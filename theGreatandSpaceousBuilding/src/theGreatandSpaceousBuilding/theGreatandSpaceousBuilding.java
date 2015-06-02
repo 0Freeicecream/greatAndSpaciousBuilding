e89317cc36deb3f2c23e2a2b3300edde90589e58
0Freeicecream <0freeicecream@gmail.com>
2015-06-02 11:19:09 -0700

    Added getter and setters for Player and Game Created display and player name stuff

--- theGreatandSpaceousBuilding/src/theGreatandSpaceousBuilding/theGreatandSpaceousBuilding.java
+++ theGreatandSpaceousBuilding/src/theGreatandSpaceousBuilding/theGreatandSpaceousBuilding.java
@@ -11,6 +11,7 @@ import byui.cit260.theGreatandSpaceousBuilding.model.Player;
 import byui.cit260.theGreatandSpaceousBuilding.model.Location;
 import byui.cit260.theGreatandSpaceousBuilding.model.Map;
 import byui.cit260.theGreatandSpaceousBuilding.model.Scenario;
+import byui.cit260.theGreatandSpaceousBuilding.view.StartProgramView;
 
 /**
  *
@@ -18,71 +19,30 @@ import byui.cit260.theGreatandSpaceousBuilding.model.Scenario;
 */
 public class theGreatandSpaceousBuilding {
     
+    private static Game currentGame = null;
+    private static Player player = null;
+    
+    private static getCurrentGame() {
+        return currentGame;
+    }
+    
+    private static setCurrentGame(Game currentGame) {
+        theGreatandSpaceousBuilding.currentGame = currentGame;
+    }
+    
+    private static getPlayer() {
+        return player;
+    }
+    
+    private static setPlayer (Player player) {
+        theGreatandSpaceousBuilding.player = player;
+    }
         
     public static void main(String[] args) {
         
-        //Game Test
-        Game gameOne = new Game();
-        gameOne.setTotalTime(0.0);
-        gameOne.setPlayerCoordinatesX(1);
-        gameOne.setPlayerCoordinatesY(5);
+        //BruceBanner Test
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.startProgram();
         
-        String gameInfo = gameOne.toString();
-        System.out.println(gameInfo);
-        
-        //Player Test
-        Player playerOne = new Player();    
-        
-        playerOne.setName("Fred Fredburger");
-        playerOne.setBestTime(999999999.00);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        //Map Test
-        Map mapOne = new Map();
-        
-        mapOne.setRowCount(5);
-        mapOne.setCollumnCount(5);
-        
-        String mapInfo = mapOne.toString();
-        System.out.println(mapInfo);
-    
-        //Location Test
-        Location locationOne = new Location();    
-        
-        locationOne.setX(0);
-        locationOne.setY(-1);
-        locationOne.setFruitPoints(2);
-        locationOne.setIronRod(true);
-        locationOne.setPath(true);
-        locationOne.setHolyGhost(false);
-        
-        String locationInfo = locationOne.toString();
-        System.out.println(locationInfo);
-        
-        //Challenge Test
-        Challenge challengeOne = new Challenge();
-        
-        challengeOne.setType("Pringles");
-        challengeOne.setObedienceMod(0);
-        challengeOne.setTestimonyMod(-7);
-        
-        String challengeInfo = challengeOne.toString();
-        System.out.println(challengeInfo);
-        
-        //Test scenario
-        Scenario scenarioOne = new Scenario();
-        
-        scenarioOne.setStory("There once was a man from Peru, who dreamed he was eating his shoe, he woke up with a fright, on that terrible night, to find out that his dream had come true");
-        scenarioOne.setQuestion("What is the answer to the equation 2 + 2?");
-        scenarioOne.setChoices("A: -10 B: -20X6 C: 2 D: Fish");
-        scenarioOne.setAnswer("C");
-        scenarioOne.setEarnedFruits(2);
-        
-        String scenarioInfo = scenarioOne.toString();
-        System.out.println(scenarioInfo);
-        
-   
     }
 }
