d46d88351349e0e239b801478b2caeeb59eb2a11
ngriley <fai11004@byui.edu>
2015-06-02 10:33:14 -0700

    added View Package, started beginning of  "startProgram()".

---
+++ theGreatandSpaceousBuilding/test/byui/cit260/theGreatandSpaceousBuilding/view/StartProgramViewTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theGreatandSpaceousBuilding.view;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Néna
+ */
+public class StartProgramViewTest {
+    
+    public StartProgramViewTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of startProgram method, of class StartProgramView.
+     */
+    @Test
+    public void testStartProgram() {
+        System.out.println("startProgram");
+        StartProgramView instance = new StartProgramView();
+        instance.startProgram();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
